8f29ea141eb3ae4c5108e07c46ff76447020ef95
Joseph Coppola <jjc9718@rit.edu>
2016-05-17 15:04:39 -0400

    Menu exists and switches between modes

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject gameInfoPrefab;
+
+	public void OnTimedClick()
+	{
+		GameTypeInfo infoToPass = Instantiate (gameInfoPrefab).GetComponent<GameTypeInfo> ();
+		infoToPass.timed = true;
+		SceneManager.LoadScene ("Game");
+	}
+
+	public void OnMovesClick()
+	{
+		GameTypeInfo infoToPass = Instantiate (gameInfoPrefab).GetComponent<GameTypeInfo> ();
+		infoToPass.timed = false;
+		SceneManager.LoadScene ("Game");
+	}
+}
